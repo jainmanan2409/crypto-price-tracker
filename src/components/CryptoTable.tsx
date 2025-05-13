4d2d9950f8748f2d40d6d313ec83683db5d40af4
Manan Jain <jainmanan2409@gmail.com>
2025-05-14 00:10:09 +0530

    initial commit

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -29,24 +29,24 @@ const CryptoTable: React.FC = () => {
     <tbody>
       {assets.map((coin, index) => (
         <tr key={coin.id}>
-          <td>{index + 1}</td>
-          <td><img src={coin.logo} alt={coin.name} className="h-6 w-6" /></td>
-          <td>{coin.name}</td>
-          <td>{coin.symbol}</td>
-          <td>${coin.price.toLocaleString()}</td>
-          <td className={coin.percent1h >= 0 ? "text-green-500" : "text-red-500"}>
+          <td data-label = "#">{index + 1}</td>
+          <td data-label = "Logo"><img src={coin.logo} alt={coin.name} className="h-6 w-6" /></td>
+          <td data-label = "Name">{coin.name}</td>
+          <td data-label = "Symbol">{coin.symbol}</td>
+          <td data-label = "Price">${coin.price.toLocaleString()}</td>
+          <td data-label="1h %" className={coin.percent1h >= 0 ? "text-green-500" : "text-red-500"}>
             {coin.percent1h.toFixed(2)}%
           </td>
-          <td className={coin.percent24h >= 0 ? "text-green-500" : "text-red-500"}>
+          <td data-label="24 %" className={coin.percent24h >= 0 ? "text-green-500" : "text-red-500"}>
             {coin.percent24h.toFixed(2)}%
           </td>
-          <td className={coin.percent7d >= 0 ? "text-green-500" : "text-red-500"}>
+          <td data-label="7d %" className={coin.percent7d >= 0 ? "text-green-500" : "text-red-500"}>
             {coin.percent7d.toFixed(2)}%
           </td>
-          <td>${coin.marketCap.toLocaleString()}</td>
-          <td>${coin.volume24h.toLocaleString()}</td>
-          <td>{coin.circulatingSupply.toLocaleString()}.{coin.symbol}</td>
-          <td className="w-[120px] h-[40px] p-1">
+          <td data-label="Market Cap">${coin.marketCap.toLocaleString()}</td>
+          <td data-label="24h Volume">${coin.volume24h.toLocaleString()}</td>
+          <td data-label="Circulating Supply">{coin.circulatingSupply.toLocaleString()}.{coin.symbol}</td>
+          <td data-label="Last 7 days"className="w-[120px] h-[40px] p-1">
   {coin.history7d && (
     <MiniChart
       data={coin.history7d}
